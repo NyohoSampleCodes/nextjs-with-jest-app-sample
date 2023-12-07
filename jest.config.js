1b650f419f4508ec79352474e4c49c3ede2274b7
北䑓如法 <algebraicallyClosedField@gmail.com>
2023-12-08 00:40:21 +0900

    Set ts-jest up

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,13 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = async () => ({
+  ...(await createJestConfig(customJestConfig)()),
+  transformIgnorePatterns: [
+    'node_modules/(?!(@orgajs/reorg-rehype|@orgajs/reorg|oast-to-hast|unist-builder|hast-util-from-parse5|hastscript|property-information|hast-util-parse-selector|space-separated-tokens|comma-separated-tokens|vfile-location|web-namespaces|rehype-stringify|hast-util-to-html|html-void-elements|zwitch|stringify-entities|character-entities-legacy|character-entities-html4|ccount|hast-util-whitespace|unified|bail|devlop|is-plain-obj|trough|vfile|vfile-message|unist-util-stringify-position|@orgajs/reorg-parse|orga|text-kit)/)',
+  ]
+})
